9c908fdd0e34a64f7399ca223421dbe938d227c6
yinchenlei <yinchenlei0113@gmail.com>
2020-04-03 07:25:39 -0700

    testing

--- 304CPSC/src/test/Screen.java
+++ 304CPSC/src/test/Screen.java
@@ -1,6 +1,8 @@
 package test;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
 
 public class Screen {
     private JPanel panel1;
@@ -8,4 +10,17 @@ public class Screen {
     private JTextArea enterQueryHereTextArea;
     private JComboBox list2;
     private JButton exampleButtonButton;
+
+    public Screen()
+    {
+        exampleButtonButton = new JButton("example button");
+        enterQueryHereTextArea = new JTextArea("enter query here！！");
+        String[] operation = {"insert" , "delete", "update"};
+        list1 = new JComboBox(operation);
+        list1.setSelectedIndex(4);
+        //list1.addActionListener(this);
+    }
+    //@Override
+  //  public void actionPerformed(ActionEvent e) {
+   // };
 }
