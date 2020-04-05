49fabe205e0a86b28b68d59957165388cd449081
Peter Le <54086406+Ptwell@users.noreply.github.com>
2020-04-05 00:30:55 -0700

    push

--- 304CPSC/src/Handlers/ViewHandlers/SelectionHandler.java
+++ 304CPSC/src/Handlers/ViewHandlers/SelectionHandler.java
@@ -1,4 +1,30 @@
 package Handlers.ViewHandlers;
-
+import javax.swing.*;
+import java.lang.*;
 public class SelectionHandler {
+    private final String selection1;
+    private final String selection2;
+    private final String option;
+    private JTable returnTable;
+
+    public SelectionHandler(String selection1, String selection2, String option) {
+        this.selection1 = selection1;
+        this.selection2 = selection2;
+        this.option = option;
+    }
+
+    public String getSelection1(){
+        return selection1;
+    }
+
+    public String getSelection2(){
+        return selection2;
+    }
+
+    public JTable join(JTable s1, JTable s2) {
+        return returnTable;
+    }
+
+
+
 }
