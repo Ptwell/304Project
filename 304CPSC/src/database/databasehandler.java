0e0a9c2f01b8d6566439e1dab31233196c218987
yinchenlei <yinchenlei0113@gmail.com>
2020-04-05 02:19:14 -0700

    connection handler for game

--- 304CPSC/src/database/databasehandler.java
+++ 304CPSC/src/database/databasehandler.java
@@ -7,10 +7,188 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+
+import model.Game;
 public class databasehandler {
     private static final String ORACLE_URL = "jdbc:oracle:thin:@localhost:1522:stu";
     private static final String EXCEPTION_TAG = "[EXCEPTION]";
     private static final String WARNING_TAG = "[WARNING]";
 
     private Connection connection = null;
+    public databasehandler() {
+        try {
+            // Load the Oracle JDBC driver
+            // Note that the path could change for new drivers
+            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+        } catch (SQLException e) {
+            System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+        }
+    }
+
+    public void close() {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+        }
+    }
+
+    public void deleteBranch(int gamID) {
+        try {
+            PreparedStatement ps = connection.prepareStatement("DELETE FROM Game WHERE GameID = ?");
+            ps.setInt(1, gamID);
+
+            int rowCount = ps.executeUpdate();
+            if (rowCount == 0) {
+                System.out.println(WARNING_TAG + " game " + gamID + " does not exist!");
+            }
+
+            connection.commit();
+
+            ps.close();
+        } catch (SQLException e) {
+            System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+            rollbackConnection();
+        }
+    }
+
+    public void insertBranch(Game model) {
+        try {
+            PreparedStatement ps = connection.prepareStatement("INSERT INTO GAME VALUES (?,?)");
+            ps.setInt(1, model.getGameID());
+            ps.setString(2, model.getGameName());
+
+            /*if (model.getPhoneNumber() == 0) {
+                ps.setNull(5, java.sql.Types.INTEGER);
+            } else {
+                ps.setInt(5, model.getPhoneNumber());
+            }**/
+
+            ps.executeUpdate();
+            connection.commit();
+
+            ps.close();
+        } catch (SQLException e) {
+            System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+            rollbackConnection();
+        }
+    }
+
+    public Game[] getBGameInfo() {
+        ArrayList<Game> result = new ArrayList<Game>();
+
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Game");
+
+//    		// get info on ResultSet
+//    		ResultSetMetaData rsmd = rs.getMetaData();
+//
+//    		System.out.println(" ");
+//
+//    		// display column names;
+//    		for (int i = 0; i < rsmd.getColumnCount(); i++) {
+//    			// get column name and print it
+//    			System.out.printf("%-15s", rsmd.getColumnName(i + 1));
+//    		}
+
+            while(rs.next()) {
+                Game model = new Game(rs.getInt("gameID"),
+                        rs.getString("game name"));
+
+                result.add(model);
+            }
+
+            rs.close();
+            stmt.close();
+        } catch (SQLException e) {
+            System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+        }
+
+        return result.toArray(new Game[result.size()]);
+    }
+
+    public void updateBranch(int id, String name) {
+        try {
+            PreparedStatement ps = connection.prepareStatement("UPDATE GAME SET GameID = ? WHERE GameName = ?");
+            ps.setString(1, name);
+            ps.setInt(2, id);
+
+            int rowCount = ps.executeUpdate();
+            if (rowCount == 0) {
+                System.out.println(WARNING_TAG + " GAME " + id + " does not exist!");
+            }
+
+            connection.commit();
+
+            ps.close();
+        } catch (SQLException e) {
+            System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+            rollbackConnection();
+        }
+    }
+
+    public boolean login(String username, String password) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+
+            connection = DriverManager.getConnection(ORACLE_URL, username, password);
+            connection.setAutoCommit(false);
+
+            System.out.println("\nConnected to Oracle!");
+            return true;
+        } catch (SQLException e) {
+            System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+            return false;
+        }
+    }
+
+    private void rollbackConnection() {
+        try  {
+            connection.rollback();
+        } catch (SQLException e) {
+            System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+        }
+    }
+
+    public void databaseSetup() {
+        dropBranchTableIfExists();
+
+        try {
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate("create_table_Game(GameID INTEGER(20), GName CHAR(20), PRIMARY_KEY(GameID));");
+            stmt.close();
+        } catch (SQLException e) {
+            System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+        }
+
+        //*BranchModel branch1 = new BranchModel("123 Charming Ave", "Vancouver", 1, "First Branch", 1234567);
+        //insertBranch(branch1);
+
+       // BranchModel branch2 = new BranchModel("123 Coco Ave", "Vancouver", 2, "Second Branch", 1234568);
+       // insertBranch(branch2);
+    }
+
+    private void dropBranchTableIfExists() {
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("select table_name from user_tables");
+
+            while(rs.next()) {
+                if(rs.getString(1).toLowerCase().equals("Game")) {
+                    stmt.execute("DROP TABLE game");
+                    break;
+                }
+            }
+
+            rs.close();
+            stmt.close();
+        } catch (SQLException e) {
+            System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+        }
+    }
 }
