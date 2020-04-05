5fa7d9e350eeef5761ef140e827fb11b3650b0ca
Peter Le <54086406+Ptwell@users.noreply.github.com>
2020-04-05 03:22:38 -0700

    can't get second screen to

--- 304CPSC/src/GUI/OnViewClick.java
+++ 304CPSC/src/GUI/OnViewClick.java
@@ -1,6 +1,7 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 
 public class OnViewClick extends JDialog {
@@ -9,16 +10,19 @@ public class OnViewClick extends JDialog {
     private JButton buttonCancel;
     private JButton projectionButton;
     private JButton selectionButton;
+    private Container returnPane;
 
     public OnViewClick() {
 
         buttonOK.addActionListener(new ActionListener() {
+           // @Override
             public void actionPerformed(ActionEvent e) {
                 onOK();
             }
         });
 
         buttonCancel.addActionListener(new ActionListener() {
+           // @Override
             public void actionPerformed(ActionEvent e) {
                 onCancel();
             }
@@ -43,20 +47,26 @@ public class OnViewClick extends JDialog {
     private void onOK() {
         // add your code here
         dispose();
+        setVisible(false);
+        setContentPane(returnPane);
+        revalidate();
+
+
 //        Screen frame = new Screen();
 //        frame.showFrame();
     }
 
     private void onCancel() {
         // add your code here if necessary
-        dispose();
+        setVisible(false);
     }
 
     public void showFrame() {
         OnViewClick dialog = new OnViewClick();
+        returnPane = getContentPane();
         dialog.setContentPane(contentPane);
-        dialog.setModal(true);
-        dialog.getRootPane().setDefaultButton(buttonOK);
+        // dialog.setModal(true);
+        // dialog.getRootPane().setDefaultButton(buttonOK);
         dialog.pack();
         dialog.setVisible(true);
         // System.exit(0);
--- 304CPSC/src/GUI/Screen.java
+++ 304CPSC/src/GUI/Screen.java
@@ -17,6 +17,7 @@ public class Screen {
             public void actionPerformed(ActionEvent e) {
                 // TODO: View Button
                 OnViewClick view = new OnViewClick();
+                JFrame v = new JFrame("Please select one of the following");
                 view.showFrame();
 
             }
@@ -48,6 +49,7 @@ public class Screen {
 
     public void showFrame() {
         JFrame frame = new JFrame("QueryHandler");
+        JOptionPane.setRootFrame(frame);
         frame.setContentPane(new Screen() .panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
