f85159bf11b64ae404d56fd07f07f0bad8766736
Peter Le <54086406+Ptwell@users.noreply.github.com>
2020-04-05 19:03:13 -0700

    more implementations

--- 304CPSC/src/GUI/TableSelection.java
+++ 304CPSC/src/GUI/TableSelection.java
@@ -7,9 +7,13 @@ public class TableSelection extends JDialog {
     private JPanel contentPane;
     private JButton buttonOK;
     private JButton buttonCancel;
-    private JComboBox Selection1;
-    private JComboBox Selection2;
-    private JComboBox Options;
+    private JComboBox selection1;
+    private String s1;
+    private JComboBox selection2;
+    private  String s2;
+    private JComboBox options;
+    private JTable table1;
+    private String optionName;
 
     public TableSelection() {
         setContentPane(contentPane);
@@ -42,6 +46,43 @@ public class TableSelection extends JDialog {
                 onCancel();
             }
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+
+        options.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                optionName = extractMethod(actionEvent);
+                performOption();
+            }
+        });
+        selection1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                s1 = extractMethod(actionEvent);
+            }
+        });
+        selection2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                s2 = extractMethod(actionEvent);
+            }
+        });
+    }
+
+    private String extractMethod(ActionEvent actionEvent) {
+        JComboBox cb = (JComboBox) actionEvent.getSource();
+        String name = (String)cb.getSelectedItem();
+        return name;
+    }
+
+    private void performOption() {
+        // TODO: update table with division/join
+        if (optionName == "Division") {
+            // todo
+        } else {
+            if (optionName == "Full Outer Join") {
+                // todo
+            }
+        }
     }
 
     private void onOK() {
